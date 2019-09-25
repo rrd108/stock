e4cac7e3266b84ab752c452b9300e80a3f5f661c
rrd <rrd@1108.cc>
2019-09-25 09:55:55 +0200

    home url is set

--- config/routes.php
+++ config/routes.php
@@ -62,7 +62,7 @@ Router::scope('/', function (RouteBuilder $routes) {
      * its action called 'display', and we pass a param to select the view file
      * to use (in this case, src/Template/Pages/home.ctp)...
      */
-    $routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
+    $routes->connect('/', ['controller' => 'Stats', 'action' => 'index']);
 
     /**
      * ...and connect the rest of 'Pages' controller's URLs.
@@ -102,6 +102,6 @@ Router::scope('/', function (RouteBuilder $routes) {
  * });
  * ```
  */
-Router::prefix('admin',function (RouteBuilder $routes){
-	$routes->fallbacks(DashedRoute::class);
+Router::prefix('admin', function (RouteBuilder $routes) {
+    $routes->fallbacks(DashedRoute::class);
 });
