783fb3bc1adbce09ffb24ba70218d04469d40f42
rrd <rrd@1108.cc>
2019-12-02 16:19:32 +0100

    get back missing pieces

---
+++ api/src/Controller/AppUsersController.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Controller;
+
+use CakeDC\Users\Controller\AppController as CakeDCAppController;
+use App\Model\Table\AppUsersTable;
+use Cake\Event\Event;
+use CakeDC\Users\Controller\Component\UsersAuthComponent;
+use CakeDC\Users\Controller\Traits\LoginTrait;
+use CakeDC\Users\Controller\Traits\RegisterTrait;
+
+class AppUsersController extends CakeDCAppController
+{
+    use LoginTrait;
+    use RegisterTrait;
+
+    public function initialize()
+    {
+        parent::initialize();
+        $this->Auth->allow('getToken');
+    }
+
+    public function beforeFilter(Event $event)
+    {
+        $this->getEventManager()->off($this->Csrf);
+        $this->Security->setConfig('unlockedActions', ['getToken']);
+    }
+
+    public function getToken()
+    {
+        $user = $this->Auth->identify();
+         // TODO get the token $user = ['token' => $user['api_key']];
+        $this->set(compact('user'));
+        $this->set('_serialize', 'user');
+    }
+}
