de7345ad040496f1af088c1b06bf21cd88cedd61
rrd <rrd@1108.cc>
2020-11-30 15:47:57 +0100

    fix cors config

--- config/cors.php
+++ config/cors.php
@@ -1,9 +1,11 @@
 <?php
 return [
-  'AllowOrigin' => 'localhost:8080',
-  'AllowMethods' => ['GET', 'POST', 'PATCH', 'OPTIONS', 'DELETE', 'PUT'],
-  'AllowCredentials' => true, // TODO do we need this?
-  'AllowHeaders' => ['Origin', 'Content-Type', 'Accept', 'X-Requested-With', 'ApiKey', 'Access-Control-Request-*'],
-  'ExposeHeaders' => ['Link'],  // TODO do we need this?
-  'MaxAge' => 300,
+  'Cors' => [
+    'AllowOrigin' => 'http://localhost:8080',
+    'AllowMethods' => ['GET', 'POST', 'PATCH', 'OPTIONS', 'DELETE', 'PUT'],
+    //'AllowCredentials' => true,                                 // TODO do we need this?
+    'AllowHeaders' => ['ApiKey', 'Content-Type'],
+    //'ExposeHeaders' => ['Link'],                              // TODO do we need this?
+    'MaxAge' => 300,
+  ]
 ];
